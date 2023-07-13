1ea91872d784f128407c6d9e7831481cd20fb885
常泽川 <66781781@qq.com>
2023-07-14 00:50:41 +0800

    expo-file-system 15.4.2 bug

--- App.tsx
+++ App.tsx
@@ -1,7 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const callback = downloadProgress => {
+    const progress = downloadProgress.totalBytesWritten / downloadProgress.totalBytesExpectedToWrite;
+    console.log(progress);
+  };
+
+  const downloadResumable = FileSystem.createDownloadResumable(
+    'http://techslides.com/demos/sample-videos/small.mp4',
+    FileSystem.cacheDirectory + 'small.mp4',
+    {},
+    callback
+  );
+
+  const load = async () => {
+    console.log("start download ========");
+    await downloadResumable.downloadAsync();
+    console.log("end download ========");
+  }
+
+  useEffect(()=> {
+    load();
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
